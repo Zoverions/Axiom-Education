0006ac73a2c22113c3ff696944246b9ec88f783f
Zoverions <zoverions@gmail.com>
2026-07-30 13:32:56 -0400

    Match rendered course subtitle in widget smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -24,7 +24,7 @@ void main() {
 
     expect(find.text('Ontario Secondary Curriculum Pack'), findsOneWidget);
     expect(find.text('3 courses • 31 expectations'), findsOneWidget);
-    expect(find.text('Arts Foundations'), findsOneWidget);
+    expect(find.textContaining('Arts Foundations'), findsOneWidget);
     expect(find.text('A'), findsNWidgets(2));
     expect(tester.takeException(), isNull);
 
